1308b6a747c8e30500ad3fb47001c74d1afe4ee1
Insaf <insaf_mechergui@outlook.com>
2020-12-08 13:14:35 +0100

    create components

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <h1>todo List</h1>
-      <header className="App-header"></header>
+      <header className="App-header">
+        <TodoList />
+      </header>
     </div>
   );
 }
